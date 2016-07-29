0ce33ba8e1e607d9c3a63b7c61e6b6eeaacd285d
Jodee90 <Jdiplas@gmail.com>
2016-07-29 12:52:38 +0300

    Added json serialize for User class

--- src/main/java/eu/openminted/registry/core/controllers/Utils.java
+++ src/main/java/eu/openminted/registry/core/controllers/Utils.java
@@ -2,12 +2,17 @@ package eu.openminted.registry.core.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import eu.openminted.registry.core.domain.Occurencies;
 import eu.openminted.registry.core.domain.Paging;
 import eu.openminted.registry.core.domain.Resource;
 import eu.openminted.registry.core.domain.ResourceType;
+import eu.openminted.registry.core.domain.User;
+
 import org.apache.log4j.Logger;
 
 import java.net.URL;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -23,6 +28,34 @@ public class Utils {
 
 		try {
 			return mapper.writeValueAsString(paging);
+		} catch (JsonProcessingException e) {
+//			logger.error("Error serializing object to json", e);
+			return e.getMessage();
+//			return null;
+		}
+	}
+	
+	
+	
+	public static String objToJson(Occurencies occurencies){
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		try {
+			return mapper.writeValueAsString(occurencies);
+		} catch (JsonProcessingException e) {
+//			logger.error("Error serializing object to json", e);
+			return e.getMessage();
+//			return null;
+		}
+	}
+	
+	public static String objToJson(List<Occurencies> occurencies){
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		try {
+			return mapper.writeValueAsString(occurencies);
 		} catch (JsonProcessingException e) {
 //			logger.error("Error serializing object to json", e);
 			return e.getMessage();
@@ -62,6 +95,19 @@ public class Utils {
 
 		try {
 			return mapper.writeValueAsString(resourceType);
+		} catch (JsonProcessingException e) {
+			logger.error("Error serializing object to json", e);
+
+			return null;
+		}
+	}
+	
+	public static String objToJson(User user){
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		try {
+			return mapper.writeValueAsString(user);
 		} catch (JsonProcessingException e) {
 			logger.error("Error serializing object to json", e);
 
