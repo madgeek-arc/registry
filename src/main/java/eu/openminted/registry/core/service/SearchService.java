42f5b58c9ca49683e07f7b694982088b914c7b11
Jodee90 <Jdiplas@gmail.com>
2016-10-31 16:01:19 +0200

    Search service - ElasticSearch implementation

--- src/main/java/eu/openminted/registry/core/service/SearchService.java
+++ src/main/java/eu/openminted/registry/core/service/SearchService.java
@@ -5,7 +5,12 @@ import eu.openminted.registry.core.domain.Paging;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import org.elasticsearch.index.query.BoolQueryBuilder;
+
 
 public interface SearchService {
+	
 	Paging search(String resourceType, String cqlQuery, int from, int to, String[] browseBy) throws ServiceException;
+	
+	Paging searchElastic(String resourceType, BoolQueryBuilder qBuilder, int from, int to, String[] browseBy) throws ServiceException;
 }
