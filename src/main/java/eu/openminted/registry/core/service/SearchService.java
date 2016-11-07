c98f3e9251917ef41087b8a22f31329602ce8ec4
Stefanos Gatsios <stevengatsios@gmail.com>
2016-11-07 16:35:44 +0200

    Upgraded to version 0.0.2-SNAPSHOT
    Introducing elasticsearch 5.0

--- src/main/java/eu/openminted/registry/core/service/SearchService.java
+++ src/main/java/eu/openminted/registry/core/service/SearchService.java
@@ -3,6 +3,7 @@ package eu.openminted.registry.core.service;
 import eu.openminted.registry.core.domain.Paging;
 
 import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 
 import org.elasticsearch.index.query.BoolQueryBuilder;
@@ -12,5 +13,5 @@ public interface SearchService {
 	
 	Paging search(String resourceType, String cqlQuery, int from, int to, String[] browseBy) throws ServiceException;
 	
-	Paging searchElastic(String resourceType, BoolQueryBuilder qBuilder, int from, int to, String[] browseBy) throws ServiceException;
+	Paging searchElastic(String resourceType, BoolQueryBuilder qBuilder, int from, int to, String[] browseBy) throws ServiceException, UnknownHostException;
 }
