ad8ae9b7ab60cf46842c9731c839e1446d10c554
Stefanos Gatsios <stevengatsios@gmail.com>
2017-06-12 13:15:40 +0300

    FacetFilter for search controller

--- src/main/java/eu/openminted/registry/core/controllers/SearchController.java
+++ src/main/java/eu/openminted/registry/core/controllers/SearchController.java
@@ -1,5 +1,7 @@
 package eu.openminted.registry.core.controllers;
 
+import eu.openminted.registry.core.domain.FacetFilter;
+import eu.openminted.registry.core.domain.Paging;
 import eu.openminted.registry.core.service.SearchService;
 import eu.openminted.registry.core.service.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.HashMap;
 
 @RestController
 public class SearchController {
@@ -19,120 +23,19 @@ public class SearchController {
 	@Autowired
 	private SearchService searchService;
 	
-	@RequestMapping(value = "/search/{name}/{query}/", method = RequestMethod.GET, headers = "Accept=application/json")
-	public ResponseEntity<String> getResourceTypeByName(@PathVariable("query") String query, @PathVariable("name") String name) {
-		ResponseEntity<String> responseEntity;
-		
+	@RequestMapping(value = "/search/{name}/{query}", method = RequestMethod.GET, headers = "Accept=application/json")
+	public ResponseEntity<Paging> getResourceTypeByName(
+			@PathVariable("name") String name,
+			@RequestParam(value = "keyword", required = false, defaultValue = "") String keyword,
+			@RequestParam(value = "from", required = false, defaultValue = "0") int from,
+			@RequestParam(value = "quantity", required = false, defaultValue = "10") int quantity,
+			@RequestParam(value = "browseBy", required = false, defaultValue = "") String[] browseBy
+	) throws ServiceException {
+		FacetFilter filter = new FacetFilter(keyword,name,from,quantity,new HashMap<>(), Arrays.asList(browseBy));
 		try {
-			responseEntity = new ResponseEntity<>(Utils.objToJson(searchService.searchKeyword(name, query, 0, 0, new String[0])), HttpStatus.OK);
-			return responseEntity;
-		} catch (ServiceException | UnknownHostException e) {
-			responseEntity = new ResponseEntity<>("{\"error\":\"" + e.getMessage() + "\"}", HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(searchService.search(filter), HttpStatus.OK);
+		} catch (UnknownHostException e) {
+			throw new ServiceException(e);
 		}
-
-
-		return responseEntity;
-	}
-	
-	
-	@RequestMapping(value = "/search/{name}/{query}/", params = {"from"}, method = RequestMethod.GET, headers = "Accept=application/json")
-	public ResponseEntity<String> getResourceTypeByName1(@PathVariable("query") String query, @PathVariable("name") String name, @RequestParam(value = "from") int from) {
-		ResponseEntity<String> responseEntity;
-		
-		try {
-			responseEntity = new ResponseEntity<>(Utils.objToJson(searchService.searchKeyword(name, query, from, 0, new String[0])), HttpStatus.OK);
-			return responseEntity;
-		} catch (ServiceException | UnknownHostException e) {
-			responseEntity = new ResponseEntity<>("{\"error\":\"" + e.getMessage() + "\"}", HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-
-		return responseEntity;
-	}
-	
-	@RequestMapping(value = "/search/{name}/{query}/", params = {"to"}, method = RequestMethod.GET, headers = "Accept=application/json")
-	public ResponseEntity<String> getResourceTypeByName2(@PathVariable("query") String query, @PathVariable("name") String name, @RequestParam(value = "to") int to ) {
-		ResponseEntity<String> responseEntity;
-		
-		try {
-			responseEntity = new ResponseEntity<>(Utils.objToJson(searchService.searchKeyword(name, query, 0, to, new String[0])), HttpStatus.OK);
-			return responseEntity;
-		} catch (ServiceException | UnknownHostException e) {
-			responseEntity = new ResponseEntity<>("{\"error\":\"" + e.getMessage() + "\"}", HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-
-		return responseEntity;
-	}
-	
-	@RequestMapping(value = "/search/{name}/{query}/", params = {"from","to"}, method = RequestMethod.GET, headers = "Accept=application/json")
-	public ResponseEntity<String> getResourceTypeByName3(@PathVariable("query") String query, @PathVariable("name") String name, @RequestParam(value="from") int from ,@RequestParam(value = "to") int to ) {
-		ResponseEntity<String> responseEntity;
-		
-		try {
-			responseEntity = new ResponseEntity<>(Utils.objToJson(searchService.searchKeyword(name, query, from, to, new String[0])), HttpStatus.OK);
-			return responseEntity;
-		} catch (ServiceException | UnknownHostException e) {
-			responseEntity = new ResponseEntity<>("{\"error\":\"" + e.getMessage() + "\"}", HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-
-		return responseEntity;
-		
-	}
-	
-	@RequestMapping(value = "/search/{name}/{query}/",params = {"browseBy"} ,method = RequestMethod.GET, headers = "Accept=application/json")
-	public ResponseEntity<String> getResourceTypeByName(@PathVariable("query") String query, @PathVariable("name") String name, @RequestParam(value="browseBy") String[] browseBy) {
-		ResponseEntity<String> responseEntity;
-		
-		try {
-			responseEntity = new ResponseEntity<>(Utils.objToJson(searchService.searchKeyword(name, query, 0, 0, browseBy)), HttpStatus.OK);
-			return responseEntity;
-		} catch (ServiceException | UnknownHostException e) {
-			responseEntity = new ResponseEntity<>("{\"error\":\"" + e.getMessage() + "\"}", HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-
-		return responseEntity;
-	}
-	
-	
-	@RequestMapping(value = "/search/{name}/{query}/", params = {"from","browseBy"}, method = RequestMethod.GET, headers = "Accept=application/json")
-	public ResponseEntity<String> getResourceTypeByName1(@PathVariable("query") String query, @PathVariable("name") String name, @RequestParam(value = "from") int from, @RequestParam(value = "browseBy") String[] browseBy) {
-		ResponseEntity<String> responseEntity;
-		
-		try {
-			responseEntity = new ResponseEntity<>(Utils.objToJson(searchService.searchKeyword(name, query, from, 0, browseBy)), HttpStatus.OK);
-			return responseEntity;
-		} catch (ServiceException | UnknownHostException e) {
-			responseEntity = new ResponseEntity<>("{\"error\":\"" + e.getMessage() + "\"}", HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-
-		return responseEntity;
-	}
-	
-	@RequestMapping(value = "/search/{name}/{query}/", params = {"to","browseBy"}, method = RequestMethod.GET, headers = "Accept=application/json")
-	public ResponseEntity<String> getResourceTypeByName2(@PathVariable("query") String query, @PathVariable("name") String name, @RequestParam(value = "to") int to, @RequestParam(value = "browseBy") String[] browseBy ) {
-		ResponseEntity<String> responseEntity;
-		
-		try {
-			responseEntity = new ResponseEntity<>(Utils.objToJson(searchService.searchKeyword(name, query, 0, to, browseBy)), HttpStatus.OK);
-			return responseEntity;
-		} catch (ServiceException | UnknownHostException e) {
-			responseEntity = new ResponseEntity<>("{\"error\":\"" + e.getMessage() + "\"}", HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-
-		return responseEntity;
-	}
-	
-	@RequestMapping(value = "/search/{name}/{query}/", params = {"from","to","browseBy"}, method = RequestMethod.GET, headers = "Accept=application/json")
-	public ResponseEntity<String> getResourceTypeByName3(@PathVariable("query") String query, @PathVariable("name") String name, @RequestParam(value="from") int from ,@RequestParam(value = "to") int to, @RequestParam(value="browseBy") String[] browseBy  ) {
-		ResponseEntity<String> responseEntity;
-		
-		try {
-			responseEntity = new ResponseEntity<>(Utils.objToJson(searchService.searchKeyword(name, query, from, to, browseBy)), HttpStatus.OK);
-			return responseEntity;
-		} catch (ServiceException | UnknownHostException e) {
-			responseEntity = new ResponseEntity<>("{\"error\":\"" + e.getMessage() + "\"}", HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-
-		return responseEntity;
-		
 	}
 }
