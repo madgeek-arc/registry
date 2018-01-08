673b81f298fa1c3eb78b91a393406180d1be6496
John Diplas <jdiplas@gmail.com>
2018-01-08 11:53:11 +0200

    Added dump and restore service functionality

---
+++ src/main/java/eu/openminted/registry/core/service/DumpService.java
@@ -0,0 +1,13 @@
+package eu.openminted.registry.core.service;
+
+import eu.openminted.registry.core.domain.Resource;
+
+import java.io.File;
+import java.util.List;
+
+public interface DumpService {
+
+	File bringAll(boolean isRaw, boolean schemaless, String[] resourceTypes);
+
+	void createDirectory(String name, List<Resource> resources, boolean isRaw);
+}
