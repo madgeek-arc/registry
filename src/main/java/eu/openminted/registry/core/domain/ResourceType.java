1fbb1a32a69fd82f25e9aabb3688738d641debfa
John Diplas <Jdiplas@gmail.com>
2018-01-31 20:21:30 +0200

    Trying...

--- src/main/java/eu/openminted/registry/core/domain/ResourceType.java
+++ src/main/java/eu/openminted/registry/core/domain/ResourceType.java
@@ -53,7 +53,7 @@ public class ResourceType {
     @JsonIgnore
     private List<Resource> resources;
 
-    @OneToMany(mappedBy = "resourceType", fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "resourceType")
 //    @JsonBackReference TODO
 //    @JsonIgnore TODO
     @JsonIgnore
