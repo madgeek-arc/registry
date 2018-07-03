83f54a2da3d32ff4c0f5681ef32032c4276764e8
John Diplas <Jdiplas@gmail.com>
2018-07-03 14:43:00 +0300

    Logging the test class

--- registry-core/src/test/java/eu/openminted/registry/component/service/TestComponentRegistryService.java
+++ registry-core/src/test/java/eu/openminted/registry/component/service/TestComponentRegistryService.java
@@ -1,13 +1,11 @@
 package eu.openminted.registry.component.service;
 
+import eu.openminted.interop.componentoverview.model.ComponentMetaData;
+import junit.framework.TestCase;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
-
-import org.jdom.Document;
-
-import eu.openminted.interop.componentoverview.model.ComponentMetaData;
-import junit.framework.TestCase;
 
 public class TestComponentRegistryService extends TestCase {
 
@@ -15,7 +13,8 @@ public class TestComponentRegistryService extends TestCase {
 		ComponentRegistryService crs = new ComponentRegistryService();
 		
 		URL jarURL = this.getClass().getClassLoader().getResource("gate-component-test.jar");
-		
+
+		System.out.println("Making a call @ " + jarURL);
 		List<ComponentMetaData> descriptions = crs.describe(jarURL);
 		
 		assertEquals(16, descriptions.size());
@@ -26,7 +25,8 @@ public class TestComponentRegistryService extends TestCase {
 		ComponentRegistryService crs = new ComponentRegistryService();
 		
 		URL jarURL = this.getClass().getClassLoader().getResource("uimaFIT-component-test.jar");
-		
+
+		System.out.println("Making a call @ " + jarURL);
 		List<ComponentMetaData> descriptions = crs.describe(jarURL);
 		
 		assertEquals(8, descriptions.size());
@@ -35,7 +35,7 @@ public class TestComponentRegistryService extends TestCase {
 	
 	public void testUimaFITMaven() throws IOException {
 		ComponentRegistryService crs = new ComponentRegistryService();
-		
+
 		List<ComponentMetaData> descriptions = crs.describe("de.tudarmstadt.ukp.dkpro.core","de.tudarmstadt.ukp.dkpro.core.stanfordnlp-gpl","1.8.0");
 		
 		assertEquals(8, descriptions.size());
