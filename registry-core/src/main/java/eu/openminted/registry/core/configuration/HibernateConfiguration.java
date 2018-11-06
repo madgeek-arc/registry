ced6d81019f592d215bbd7af0ae533e511f5b446
Jodee90 <jdiplas@gmail.com>
2018-11-06 12:23:16 +0200

    Merged develop and register-core-client

--- registry-core/src/main/java/eu/openminted/registry/core/configuration/HibernateConfiguration.java
+++ registry-core/src/main/java/eu/openminted/registry/core/configuration/HibernateConfiguration.java
@@ -1,6 +1,7 @@
 package eu.openminted.registry.core.configuration;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.flywaydb.core.Flyway;
@@ -23,7 +24,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.FlushModeType;
 import javax.sql.DataSource;
-import java.beans.PropertyVetoException;
 import java.util.Properties;
 
 @Configuration
@@ -89,45 +89,35 @@ public class HibernateConfiguration {
         return transactionManager;
     }
 
-	@Bean(destroyMethod="close")
-	public DataSource dataSource(){
-		ComboPooledDataSource dataSource = new ComboPooledDataSource();
-////		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//		dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
-//		dataSource.setUrl(environment.getRequiredProperty("jdbc.url")+"?autoReconnect=true");
-//		dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
-//		dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
-		try {
-			dataSource.setDriverClass(environment.getRequiredProperty("jdbc.driverClassName"));
-		} catch (PropertyVetoException e) {
-			logger.error(e.getMessage(),e);
-		}
-		dataSource.setJdbcUrl(environment.getRequiredProperty("jdbc.url"));
-		dataSource.setUser(environment.getRequiredProperty("jdbc.username"));
-		dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
-		dataSource.setAcquireIncrement(2);
-		dataSource.setMinPoolSize(2);
-		dataSource.setMaxPoolSize(10);
-		dataSource.setMaxIdleTime(60);//seconds
-		dataSource.setPreferredTestQuery("SELECT 1");
-		dataSource.setDebugUnreturnedConnectionStackTraces(true);
-		dataSource.setTestConnectionOnCheckout(true);
-		dataSource.setTestConnectionOnCheckin(true);
-		dataSource.setIdleConnectionTestPeriod(10);
-		dataSource.setInitialPoolSize(2);
-		dataSource.setMaxStatements(0);
-		dataSource.setMaxStatementsPerConnection(0);
-		dataSource.setPrivilegeSpawnedThreads(true);
-        try {
-            dataSource.setContextClassLoaderSource("library");
-        } catch (PropertyVetoException e) {
-            logger.error("Unable to create context class load for c3p0",e);
-        }
+    @Bean
+    HikariConfig hikariConfig(){
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setPoolName("RegistryCP");
+        hikariConfig.setConnectionTestQuery("SELECT 1");
+        hikariConfig.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
+        hikariConfig.setMaximumPoolSize(20);
+        hikariConfig.setIdleTimeout(60);
+        hikariConfig.setJdbcUrl(environment.getRequiredProperty("jdbc.url"));
+        hikariConfig.setUsername(environment.getRequiredProperty("jdbc.username"));
+        hikariConfig.setPassword(environment.getRequiredProperty("jdbc.password"));
+        hikariConfig.addDataSourceProperty("cachePreStmts", "true"); // Enable Prepared Statement caching
+        hikariConfig.addDataSourceProperty("prepStmtCacheSize", "25"); // How many PS cache, default: 25
+        hikariConfig.addDataSourceProperty("useServerPrepStmts", "true"); // If supported use PS server-side
+        hikariConfig.addDataSourceProperty("useLocalSessionState", "true"); // Enable setAutoCommit
+        hikariConfig.addDataSourceProperty("useLocalTransactionState", "true"); // Enable commit/rollbacks
 
-        return dataSource;
-	}
 
-	private Properties hibernateProperties() {
+        return hikariConfig;
+    }
+
+
+    @Bean
+    public DataSource dataSource(){
+        return new HikariDataSource(hikariConfig());
+    }
+
+
+    private Properties hibernateProperties() {
 		Properties properties = new Properties();
 		properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
 		properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
