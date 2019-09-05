1bc885fe50a6439edce633c56feed1ab9db3c038
Jodee90 <jdiplas@gmail.com>
2019-09-05 16:26:23 +0300

    Trying just Autowiring the context

--- registry-core/src/main/java/eu/openminted/registry/core/index/IndexMapperFactory.java
+++ registry-core/src/main/java/eu/openminted/registry/core/index/IndexMapperFactory.java
@@ -1,25 +1,18 @@
 package eu.openminted.registry.core.index;
 
 import eu.openminted.registry.core.domain.ResourceType;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Repository;
 
 /**
  * Created by antleb on 5/21/16.
  */
 @Repository("indexMapperFactory")
-public class IndexMapperFactory implements ApplicationContextAware {
+public class IndexMapperFactory{
 
 	@Autowired
 	private ApplicationContext context;
-
-	@Override
-	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		this.context = applicationContext;
-	}
 
 	public IndexMapper createIndexMapper(ResourceType resourceType) throws Exception {
 //		IndexMapper indexMapper = null;
