e703486e9c6ecceaf60f420d6d9a7d43e2d29d54
Konstantinos Spyrou <spyroukon@gmail.com>
2021-11-26 17:59:38 +0200

    fixed JSONFieldParser when value is JSONArray

--- registry-core/src/main/java/eu/openminted/registry/core/index/JSONFieldParser.java
+++ registry-core/src/main/java/eu/openminted/registry/core/index/JSONFieldParser.java
@@ -10,25 +10,25 @@ import java.util.stream.Collectors;
 
 @Component
 public class JSONFieldParser implements FieldParser {
-	
-	public Set<Object> parse(String payload, String fieldType, String path, boolean isMultiValued) {
 
-		Set<Object> response = null;
-		if(isMultiValued){
-			List<String> answers = JsonPath.read(payload, path);
-			if (answers != null) {
-				response = answers
-						.stream()
-						.map(answer -> FieldParser.parseField(fieldType,answer))
-						.flatMap(Collection::stream)
-						.collect(Collectors.toSet());
-			}
-		}else{
-			Object answer = JsonPath.read(payload + "", path);
-			if (answer != null) {
-				response = FieldParser.parseField(fieldType, answer.toString());
-			}
-		}
-		return response;
-	}
+    public Set<Object> parse(String payload, String fieldType, String path, boolean isMultiValued) {
+
+        Set<Object> response = null;
+        if (isMultiValued) {
+            List<Object> answers = JsonPath.read(payload, path);
+            if (answers != null) {
+                response = answers
+                        .stream()
+                        .map(answer -> FieldParser.parseField(fieldType, String.valueOf(answer)))
+                        .flatMap(Collection::stream)
+                        .collect(Collectors.toSet());
+            }
+        } else {
+            Object answer = JsonPath.read(payload + "", path);
+            if (answer != null) {
+                response = FieldParser.parseField(fieldType, answer.toString());
+            }
+        }
+        return response;
+    }
 }
