b7a379daba49ad58d379a089c7194e82cf607140
Konstantinos Spyrou <spyroukon@gmail.com>
2022-11-07 19:41:27 +0200

    fixed returning null value when json indexed field is missing

--- registry-core/src/main/java/eu/openminted/registry/core/index/JSONFieldParser.java
+++ registry-core/src/main/java/eu/openminted/registry/core/index/JSONFieldParser.java
@@ -2,6 +2,8 @@ package eu.openminted.registry.core.index;
 
 import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.PathNotFoundException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
@@ -12,25 +14,29 @@ import java.util.stream.Collectors;
 @Component
 public class JSONFieldParser implements FieldParser {
 
+    private static final Logger logger = LogManager.getLogger(JSONFieldParser.class);
+
     public Set<Object> parse(String payload, String fieldType, String path, boolean isMultiValued) {
 
         Set<Object> response = null;
-        if (isMultiValued) {
-            List<Object> answers = JsonPath.read(payload, path);
-            if (answers != null) {
-                response = answers
-                        .stream()
-                        .map(answer -> FieldParser.parseField(fieldType, String.valueOf(answer)))
-                        .flatMap(Collection::stream)
-                        .collect(Collectors.toSet());
-            }
-        } else {
-            try {
+        try {
+            if (isMultiValued) {
+                List<Object> answers = JsonPath.read(payload, path);
+                if (answers != null) {
+                    response = answers
+                            .stream()
+                            .map(answer -> FieldParser.parseField(fieldType, String.valueOf(answer)))
+                            .flatMap(Collection::stream)
+                            .collect(Collectors.toSet());
+                }
+            } else {
                 Object answer = JsonPath.read(payload + "", path);
                 if (answer != null) {
                     response = FieldParser.parseField(fieldType, answer.toString());
                 }
-            } catch (PathNotFoundException e) {}
+            }
+        } catch (PathNotFoundException e) {
+            logger.debug(e);
         }
         return response;
     }
