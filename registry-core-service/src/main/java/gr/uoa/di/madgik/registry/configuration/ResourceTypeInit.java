3e0d6cf8fce37f0f985f54537bf136f41e897d2c
Konstantinos Spyrou <spyroukon@gmail.com>
2024-09-27 18:03:27 +0300

    refactored class

--- registry-core-service/src/main/java/gr/uoa/di/madgik/registry/configuration/ResourceTypeInit.java
+++ registry-core-service/src/main/java/gr/uoa/di/madgik/registry/configuration/ResourceTypeInit.java
@@ -3,14 +3,14 @@ package gr.uoa.di.madgik.registry.configuration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gr.uoa.di.madgik.registry.domain.ResourceType;
 import gr.uoa.di.madgik.registry.service.ResourceTypeService;
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.core.io.support.ResourcePatternUtils;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
-import jakarta.annotation.PostConstruct;
 import java.io.IOException;
 import java.util.Date;
 
@@ -19,28 +19,47 @@ public class ResourceTypeInit {
 
     private static final Logger logger = LoggerFactory.getLogger(ResourceTypeInit.class);
 
-    private final ResourceLoader resourceLoader;
     private final ResourceTypeService resourceTypeService;
     private final ObjectMapper mapper = new ObjectMapper();
+    private final String resourceTypesLocation;
 
 
-    public ResourceTypeInit(ResourceLoader resourceLoader, ResourceTypeService resourceTypeService) {
-        this.resourceLoader = resourceLoader;
+    public ResourceTypeInit(@Value("${registry.resource-type-init.location:classpath:resourceTypes}") String resourceTypesLocation,
+                            ResourceTypeService resourceTypeService) {
+        this.resourceTypesLocation = resourceTypesLocation;
         this.resourceTypeService = resourceTypeService;
     }
 
     @PostConstruct
     void addResourceTypes() {
-        Resource[] resources = loadResources("file:resourceTypes/*.json");
-        if (resources != null) {
+        String pattern = getLocationPattern();
+        Resource[] resources = loadResources(pattern);
+        if (resources == null || resources.length == 0) {
+            logger.info("No Resource Types found under '{}'", pattern);
+        } else {
             for (Resource resource : resources) {
                 try {
                     addResourceTypeFromFile(resource);
                 } catch (IOException e) {
                     logger.error(String.format("Could not add Resource Type from file [filename=%s]", resource.getFilename()), e);
+                } catch (Exception e) {
+                    logger.error("Invalid format of resourceType '{}'", resource.getFilename(), e);
                 }
             }
         }
+    }
+
+    /**
+     * <p>Creates the location to search for ResourceType (.json) files.</p>
+     * <p>Makes sure to check for JSON file and removes duplicate // from the path.</p>
+     *
+     * @return
+     */
+    private String getLocationPattern() {
+        String location = resourceTypesLocation;
+        if (!resourceTypesLocation.endsWith(".json"))
+            location += "/*.json";
+        return location.replaceAll("/{2,}", "/");
     }
 
     /**
@@ -52,7 +71,8 @@ public class ResourceTypeInit {
      */
     private Resource[] loadResources(String pattern) {
         try {
-            return ResourcePatternUtils.getResourcePatternResolver(resourceLoader).getResources(pattern);
+            PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+            return resolver.getResources(pattern);
         } catch (IOException e) {
             logger.error("Could not load resourceTypes from '{}'", pattern, e);
         }
