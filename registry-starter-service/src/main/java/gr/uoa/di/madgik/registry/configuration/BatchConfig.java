a0affb7aa5141b00267718e00312b5c88fe1f422
Konstantinos Spyrou <spyroukon@gmail.com>
2025-02-07 18:19:55 +0200

    refactor: remove jobRegistry post processor

--- registry-starter-service/src/main/java/gr/uoa/di/madgik/registry/configuration/BatchConfig.java
+++ registry-starter-service/src/main/java/gr/uoa/di/madgik/registry/configuration/BatchConfig.java
@@ -1,8 +1,6 @@
 package gr.uoa.di.madgik.registry.configuration;
 
-import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.support.JobRegistrySmartInitializingSingleton;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.TaskExecutorJobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
@@ -18,14 +16,6 @@ import org.springframework.web.context.WebApplicationContext;
         dataSourceRef = "registryDataSource",
         transactionManagerRef = "registryTransactionManager")
 public class BatchConfig {
-
-    @Bean
-    @ConditionalOnMissingBean
-    public JobRegistrySmartInitializingSingleton jobRegistryBeanPostProcessor(JobRegistry jobRegistry) {
-        JobRegistrySmartInitializingSingleton postProcessor = new JobRegistrySmartInitializingSingleton();
-        postProcessor.setJobRegistry(jobRegistry);
-        return postProcessor;
-    }
 
     @Bean
     @ConditionalOnMissingBean(name = "jobLauncher")
