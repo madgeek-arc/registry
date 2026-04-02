6f0ab16a4dcc67e54061df11597cf795f59ae085
Konstantinos Spyrou <spyroukon@gmail.com>
2026-04-02 13:38:56 +0300

    refactor: support date strings in elastic mapping alongside epoch

--- registry-starter-elasticsearch/src/main/java/gr/uoa/di/madgik/registry/elasticsearch/service/ElasticOperationsService.java
+++ registry-starter-elasticsearch/src/main/java/gr/uoa/di/madgik/registry/elasticsearch/service/ElasticOperationsService.java
@@ -82,7 +82,7 @@ public class ElasticOperationsService implements IndexOperationsService {
     }
 
     private static final Map<String, Object> TYPE_MAP = Map.of("type", "keyword");
-    private static final Map<String, Object> DATE_MAP = Map.of("type", "date", "format", "epoch_millis");
+    private static final Map<String, Object> DATE_MAP = Map.of("type", "date", "format", "strict_date_optional_time||epoch_millis");
     private static final Map<String, Object> TEXT_MAP = Map.of("type", "text");
     private static final Map<String, Object> DENSE_VECTOR_MAP = Map.of("type", "dense_vector", "dims", VECTOR_SIZE);
 
@@ -254,7 +254,6 @@ public class ElasticOperationsService implements IndexOperationsService {
                 Map<String, Object> typeMap = new HashMap<>();
                 typeMap.put("type", FIELD_TYPES_MAP.get(indexField.getType()));
                 switch (indexField.getType()) {
-                    case "java.util.Date", "java.time.Instant" -> typeMap.put("format", "epoch_millis");
                     case "java.lang.String" -> typeMap.put("fields", Map.of("analyzed", TEXT_MAP));
                     case "embedding" -> typeMap.put("dims", VECTOR_SIZE);
                     default -> {
