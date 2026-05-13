c9ad8c1566f227705ba4e5a036da899caa899a73
Konstantinos Spyrou <spyroukon@gmail.com>
2026-05-13 11:21:21 +0300

    fix: use readAllBytes in SchemaInput

--- registry-starter-service/src/main/java/gr/uoa/di/madgik/registry/validation/SchemaInput.java
+++ registry-starter-service/src/main/java/gr/uoa/di/madgik/registry/validation/SchemaInput.java
@@ -91,13 +91,8 @@ public class SchemaInput implements LSInput {
     public String getStringData() {
         synchronized (inputStream) {
             try {
-                byte[] input = new byte[inputStream.available()];
-                inputStream.read(input);
-                String contents = new String(input);
-                return contents;
+                return new String(inputStream.readAllBytes());
             } catch (IOException e) {
-                e.printStackTrace();
-                System.out.println("Exception " + e);
                 return null;
             }
         }
